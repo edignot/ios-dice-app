60e5e3daf2bf0e54fa990989c4f4772c61f5ff29
Ed Ignot <edignot@gmail.com>
2021-04-20 21:40:07 -0600

    Display random dices

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageOne: UIImageView!
+    
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageOne.image = diceImages.randomElement()
+        diceImageTwo.image = diceImages.randomElement()
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageOne.image = diceImages.randomElement()
+        diceImageTwo.image = diceImages.randomElement()
+        
+        // print random number 1 to 10
+        print(Int.random(in: 1...10))
+    }
+    
 }
 
